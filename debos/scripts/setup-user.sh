d912714c27b540970615a2b9589731d2f70c86e5
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2018-12-13 22:00:23 +0100

    debos: setup-user: Add to the audio group to fix "Dummy Output" on audio
    The user should have access to the audio in order to play sounds.
    
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- debos/scripts/setup-user.sh
+++ debos/scripts/setup-user.sh
@@ -6,5 +6,7 @@ adduser --gecos "$USER" \
 	--disabled-password \
 	--shell /bin/bash \
 	"$USER"
+# Add to the audio group to fix “Dummy Output” on audio
+adduser "$USER" audio
 adduser "$USER" sudo
 echo "$USER:$USER" | chpasswd
