c7e6686f53b5476bb711993838682b35cf5382ad
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2018-12-13 21:56:46 +0100

    debos: setup-user: Fix coding style, align multi-line arguments
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- debos/scripts/setup-user.sh
+++ debos/scripts/setup-user.sh
@@ -3,8 +3,8 @@
 USER=${1-debian}
 
 adduser --gecos "$USER" \
-	  --disabled-password \
-	    --shell /bin/bash \
-	      "$USER"
+	--disabled-password \
+	--shell /bin/bash \
+	"$USER"
 adduser "$USER" sudo
 echo "$USER:$USER" | chpasswd
