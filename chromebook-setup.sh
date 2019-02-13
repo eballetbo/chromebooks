eballetbo/chromebooks

commit 50da924eceb98ec9b097a60eafcafbd6c3d1fd96

Add initial support for x86_64 based Chromebooks
Support the x86_64 architecture. After this commit the tool is not more
an ARM specific tool. The x86_64 architecture was tested on a Pixel
Chromebook 2015.

Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

chromebook-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # This file:
 #
-#  - ARM Chromebook developer tool to create a Debian bootable media device.
+#  - Chromebook developer tool to create a Debian bootable media device.
 #
 # Usage:
 #
@@ -23,7 +23,7 @@ print_usage_exit()
     local arg_ret="${1-1}"
 
     echo "
-ARM/ARM64 Chromebook developer tool.
+Chromebook developer tool.
 
 Environment variables:
 
@@ -53,7 +53,7 @@ Options:
       /srv/nfs/rootfs for a NFS mount point.
 "
 echo "  --architecture=ARCH
-    Chromebook architecture, needs to be one of the following: arm | arm64"
+    Chromebook architecture, needs to be one of the following: arm | arm64 | x86_64"
 
 echo "Supported devices:
 
@@ -108,6 +108,9 @@ echo "Available commands:
 
   deploy_kernel_modules
     Install the Linux kernel modules on the rootfs.
+
+  build_bootstub
+    Build the ChromeOS bootstub.efi.
 
   build_vboot
     Build vboot image.
@@ -181,12 +184,14 @@ else
     storage_is_media_device=false
 fi
 
-[ "$CB_SETUP_ARCH" = "arm" ] || [ "$CB_SETUP_ARCH" == "arm64" ] || {
+[ "$CB_SETUP_ARCH" = "arm" ] || [ "$CB_SETUP_ARCH" == "arm64" ] || [ "$CB_SETUP_ARCH" == "x86_64" ] || {
     echo "Incorrect architecture device passed to the --architecture option."
     print_usage_exit
 }
 
-if [ "$CB_SETUP_ARCH" == "arm64" ]; then
+if [ "$CB_SETUP_ARCH" == "x86_64" ]; then
+    DEBIAN_ROOTFS_URL="$X86_64_DEBIAN_ROOTFS_URL"
+elif [ "$CB_SETUP_ARCH" == "arm64" ]; then
     DEBIAN_ROOTFS_URL="$ARM64_DEBIAN_ROOTFS_URL"
     TOOLCHAIN="$ARM64_TOOLCHAIN"
     TOOLCHAIN_URL="$ARM64_TOOLCHAIN_URL"
@@ -264,23 +269,27 @@ wait_for_partitions_to_appear()
 
 create_fit_image()
 {
-    # Devicetree binaries
-    local dtbs=""
+    if [ "$CB_SETUP_ARCH" != "x86_64" ]; then
+         # Devicetree binaries
+         local dtbs=""
 
-    # Compress image
-    rm -f arch/${CB_SETUP_ARCH}/boot/Image.lz4 || true
-    lz4 arch/${CB_SETUP_ARCH}/boot/Image arch/${CB_SETUP_ARCH}/boot/Image.lz4
+         # Compress image
+         rm -f arch/${CB_SETUP_ARCH}/boot/Image.lz4 || true
+         lz4 arch/${CB_SETUP_ARCH}/boot/Image arch/${CB_SETUP_ARCH}/boot/Image.lz4
 
-    if [ "$CB_SETUP_ARCH" == "arm" ]; then
-        dtbs="-b arch/arm/boot/dts/rk3288-veyron-minnie.dtb \
-              -b arch/arm/boot/dts/rk3288-veyron-jerry.dtb"
+         if [ "$CB_SETUP_ARCH" == "arm" ]; then
+             dtbs="-b arch/arm/boot/dts/rk3288-veyron-minnie.dtb \
+                   -b arch/arm/boot/dts/rk3288-veyron-jerry.dtb"
+         else
+             dtbs="-b arch/arm64/boot/dts/rockchip/rk3399-gru-kevin.dtb"
+         fi
+
+         mkimage -D "-I dts -O dtb -p 2048" -f auto -A arm64 -O linux -T kernel -C lz4 -a 0 \
+                 -d arch/arm64/boot/Image.lz4 $dtbs \
+                 kernel.itb
     else
-        dtbs="-b arch/arm64/boot/dts/rockchip/rk3399-gru-kevin.dtb"
+	echo "TODO: create x86_64 FIT image, now using a raw image"
     fi
-
-    mkimage -D "-I dts -O dtb -p 2048" -f auto -A arm64 -O linux -T kernel -C lz4 -a 0 \
-            -d arch/arm64/boot/Image.lz4 $dtbs \
-            kernel.itb
 }
 
 # -----------------------------------------------------------------------------
@@ -368,6 +377,11 @@ cmd_setup_rootfs()
 
 cmd_get_toolchain()
 {
+    if [ "$CB_SETUP_ARCH" == "x86_64" ]; then
+        echo "Using default distro toolchain"
+        return 0
+    fi
+
     [ -d "$TOOLCHAIN" ] && {
         echo "Toolchain already downloaded: $TOOLCHAIN"
         return 0
@@ -408,9 +422,11 @@ cmd_config_kernel()
     if [ "$CB_SETUP_ARCH" == "arm" ]; then
         scripts/kconfig/merge_config.sh -m arch/arm/configs/multi_v7_defconfig $CWD/fragments/multi-v7/chromebooks.cfg
         make olddefconfig
-    else
+    elif [ "$CB_SETUP_ARCH" == "arm64" ]; then
         scripts/kconfig/merge_config.sh -m arch/arm64/configs/defconfig $CWD/fragments/arm64/chromebooks.cfg
         make olddefconfig
+    else
+        make defconfig
     fi
 
     cd - > /dev/null
@@ -445,21 +461,45 @@ cmd_deploy_kernel_modules()
     cd kernel
 
     # Install the kernel modules on the rootfs
-    sudo make modules_install ARCH=$CB_SETUP_ARCH INSTALL_MOD_PATH=$ROOTFS_DIR
+    sudo make modules_install INSTALL_MOD_PATH=$ROOTFS_DIR
 
     cd - > /dev/null
 
     echo "Done."
 }
 
+cmd_build_bootstub()
+{
+   echo "Build bootstub.efi..."
+
+   cd bootstub
+
+   make PREFIX=""
+
+   cd - > /dev/null
+
+   echo "Done."
+}
+
 cmd_build_vboot()
 {
     echo "Sign the kernels to boot with Chrome OS devices..."
 
-    # Install it on the boot partition
-    echo "init=/sbin/init root=PARTUUID=%U/PARTNROFF=1 rootwait rw noinitrd" > boot_params
     local boot=kernel/kernel.vboot
-    sudo vbutil_kernel --pack "$boot" --keyblock /usr/share/vboot/devkeys/kernel.keyblock --version 1 --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk --bootloader boot_params --config boot_params --vmlinuz kernel/kernel.itb --arch arm
+    # Install it on the boot partition
+    if [ "$CB_SETUP_ARCH" == "x86_64" ]; then
+        echo "root=PARTUUID=%U/PARTNROFF=1 rootwait rw" > boot_params
+        [ -f bootstub/bootstub.efi ] || cmd_build_bootstub
+        sudo vbutil_kernel --pack "$boot" \
+                           --keyblock /usr/share/vboot/devkeys/kernel.keyblock \
+                           --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk \
+                           --version 1 --config boot_params \
+                           --bootloader ./bootstub/bootstub.efi \
+                           --vmlinuz kernel/arch/x86/boot/bzImage
+    else
+        echo "init=/sbin/init root=PARTUUID=%U/PARTNROFF=1 rootwait rw noinitrd" > boot_params
+        sudo vbutil_kernel --pack "$boot" --keyblock /usr/share/vboot/devkeys/kernel.keyblock --version 1 --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk --bootloader boot_params --config boot_params --vmlinuz kernel/kernel.itb --arch arm
+    fi
 
     echo "Done."
 }
@@ -475,7 +515,11 @@ cmd_deploy_vboot()
         local boot="$CB_SETUP_STORAGE1"
         sudo dd if=kernel/kernel.vboot of="$boot" bs=4M
     else
-        sudo cp -av kernel/kernel.itb "$ROOTFS_DIR/boot"
+        if [ "$CB_SETUP_ARCH" != "x86_64" ]; then
+            sudo cp -av kernel/kernel.itb "$ROOTFS_DIR/boot"
+	else
+            echo "WARNING: Not implemented for x86_64."
+	fi
     fi
 
     echo "Done."

bootstub/Makefile
@@ -1,0 +1,83 @@
+#
+#  Copyright (C) 1999-2001 Hewlett-Packard Co.
+#	Contributed by David Mosberger <davidm@hpl.hp.com>
+#	Contributed by Stephane Eranian <eranian@hpl.hp.com>
+#
+# This file is part of the gnu-efi package.
+#
+#  GNU-EFI is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  GNU-EFI is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with GNU-EFI; see the file COPYING.  If not, write to the Free
+#  Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+#  02111-1307, USA.
+#
+
+PREFIX 	   ?= /usr/bin/x86_64-pc-linux-gnu-
+CC    	   ?= $(PREFIX)gcc
+LD         ?= $(PREFIX)ld
+OBJCOPY    ?= $(PREFIX)objcopy
+
+
+EFIINC     = /usr/include/efi
+EFILIB     = /usr/lib
+EFICRT0    = /usr/lib
+
+ARCH       = $(shell uname -m | sed s,i[3456789]86,ia32,)
+
+INCDIR     = -I$(EFIINC) -I$(EFIINC)/$(ARCH)
+
+CPPFLAGS   = -DCONFIG_$(ARCH) 
+
+OPTIMFLAGS = -O2 
+DEBUGFLAGS = -Wall
+CFLAGS     = $(OPTIMFLAGS) -fpic -fshort-wchar $(DEBUGFLAGS)
+CFLAGS     += -DEFI_FUNCTION_WRAPPER
+LDFLAGS    = -nostdlib -znocombreloc
+
+
+CRTOBJS       = $(EFICRT0)/crt0-efi-$(ARCH).o
+LDSCRIPT      = $(EFICRT0)/elf_$(ARCH)_efi.lds
+
+LDFLAGS		+= -T $(LDSCRIPT) -shared -Bsymbolic -L$(EFILIB) $(CRTOBJS)
+LOADLIBES	= -lefi -lgnuefi $(shell $(CC) -print-libgcc-file-name)
+FORMAT		= efi-app-$(ARCH)
+
+
+TARGETS = bootstub.efi
+
+all:	$(TARGETS)
+
+clean:
+	rm -f $(TARGETS) *~ *.o *.so
+
+install: bootstub.efi
+	mkdir -p $(DESTDIR)
+	cp -f $^ $(DESTDIR)
+
+.PHONY: all clean install
+
+
+bootstub.so: bootstub.o trampoline.o
+	$(LD) $(LDFLAGS) $^ -o $@ $(LOADLIBES)
+
+%.efi: %.so 
+	$(OBJCOPY) -j .text -j .sdata -j .data -j .dynamic -j .dynsym -j .rel \
+		   -j .rela -j .reloc --target=$(FORMAT) $*.so $@
+
+%.so: %.o 
+	$(LD) $(LDFLAGS) $^ -o $@ $(LOADLIBES)
+
+%.o: %.c
+	$(CC) $(INCDIR) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
+
+%.o: %.S 
+	$(CC) $(INCDIR) $(CFLAGS) $(CPPFLAGS) -c $< -o $@

bootstub/README.chromium
@@ -1,0 +1,16 @@
+This is the Chrome OS bootstub that handles the handoff from our custom BIOS
+to the kernel.
+
+The Makefile was adapted from sample code in the gnu-efi library (GPLv2 or
+later).
+
+The trampoline.S code was adapted from grub v1.97 (GPLv3 or later).
+
+The bootstub.c code was written from scratch, although code from both grub
+and elilo v3.7 (GPLv2 or later) was referred to for examples.
+
+Accordingly, the bootstub code as a whole is subject to the license terms of
+the GPLv3 (or later).
+
+The authors are grateful to the above mentioned projects for their
+contributions to the free software community.

bootstub/bootstub.c
@@ -1,0 +1,252 @@
+// Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <efi.h>
+#include <efilib.h>
+
+// Here's a struct to locate the pointers we need to update in the zeropage
+// structure. Everything else should already be hardcoded at compile time.
+// See arch/x86/include/asm/bootparam.h in the kernel source for the real deal.
+struct hacked_params {
+    uint8_t  pad0[0x1c0];
+    uint32_t v0206_efi_signature;          /* 1c0 */
+    uint32_t v0206_efi_system_table;       /* 1c4 */
+    uint32_t v0206_efi_mem_desc_size;      /* 1c8 */
+    uint32_t v0206_efi_mem_desc_version;   /* 1cc */
+    uint32_t v0206_efi_mmap;               /* 1d0 */
+    uint32_t v0206_efi_mmap_size;          /* 1d4 */
+    uint32_t v0206_efi_system_table_hi;    /* 1d8 */
+    uint32_t v0206_efi_mmap_hi;            /* 1dc */
+    uint8_t  pad1[0x214 - 0x1e0];
+    uint32_t code32_start;                 /* 214 */
+    uint8_t  pad2[0x228 - 0x218];
+    uint32_t cmd_line_ptr;                 /* 228 */
+} __attribute__ ((packed));
+
+
+// Find where the preloaded params struct is located in RAM. At the moment
+// we're assuming that it immediately precedes the start of the bootstub,
+// aligned to a 4K boundary, because that's where our build system puts it.
+struct hacked_params *find_params_struct(UINTN bootstub_location)
+{
+    return (struct hacked_params *)(bootstub_location - 0x1000);
+}
+
+// Copy a string to the right within a buffer.
+static void shove_over(char *src, char *dst)
+{
+    int i = 0;
+    for (i=0; src[i]; i++)
+        ;                               // find strlen(src)
+    dst += i;
+    src += i;
+    i++;                                // also terminating '\0';
+    while (i--)
+        *dst-- = *src--;
+}
+
+// sprintf(dst,"%02x",val)
+static void one_byte(char *dst, uint8_t val)
+{
+    dst[0] = "0123456789abcdef"[(val >> 4) & 0x0F];
+    dst[1] = "0123456789abcdef"[val & 0x0F];
+}
+
+// Display a GUID in canonical form
+static void emit_guid(char *dst, uint8_t *guid)
+{
+    one_byte(dst, guid[3]); dst += 2;
+    one_byte(dst, guid[2]); dst += 2;
+    one_byte(dst, guid[1]); dst += 2;
+    one_byte(dst, guid[0]); dst += 2;
+    *dst++ = '-';
+    one_byte(dst, guid[5]); dst += 2;
+    one_byte(dst, guid[4]); dst += 2;
+    *dst++ = '-';
+    one_byte(dst, guid[7]); dst += 2;
+    one_byte(dst, guid[6]); dst += 2;
+    *dst++ = '-';
+    one_byte(dst, guid[8]); dst += 2;
+    one_byte(dst, guid[9]); dst += 2;
+    *dst++ = '-';
+    one_byte(dst, guid[10]); dst += 2;
+    one_byte(dst, guid[11]); dst += 2;
+    one_byte(dst, guid[12]); dst += 2;
+    one_byte(dst, guid[13]); dst += 2;
+    one_byte(dst, guid[14]); dst += 2;
+    one_byte(dst, guid[15]); dst += 2;
+}
+
+
+// Replace any %D with the device letter, and replace any %P with the partition
+// number. For example, ("root=/dev/sd%D%P",2,3) gives "root=/dev/sdc3".
+// Replace any %U with the human-readable form of the GUID (if provided). The
+// input string must be mutable and end with a trailing '\0', and have enough
+// room for all the expansion.
+static void update_cmdline_inplace(char *src, int devnum, int partnum,
+                                   uint8_t *guid)
+{
+    char *dst;
+
+    // Use sane values (sda3) for ridiculous inputs.
+    if (devnum < 0 || devnum > 25 || partnum < 1 || partnum > 99)
+    {
+        devnum = 0;
+        partnum = 3;
+    }
+
+    for( dst = src; *src; src++, dst++ )
+    {
+        if ( src[0] == '%' )
+        {
+            switch (src[1])
+            {
+            case 'P':
+                if (partnum > 9)
+                    *dst++ = '0' + (partnum / 10);
+                *dst = '0' + partnum % 10;
+                src++;
+                break;
+            case 'D':
+                *dst = 'a' + devnum;
+                src++;
+                break;
+            case 'U':
+                if (guid) {
+                    shove_over(src+2, dst+36);
+                    emit_guid(dst, guid);
+                    src = dst+35;
+                    dst += 35;
+                }
+            default:
+                *dst = *src;
+            }
+        }
+        else if (dst != src)
+            *dst = *src;
+    }
+    *dst = '\0';
+}
+
+// This is handy to write status codes to the LEDs for debugging.
+static __inline void port80w (unsigned short int value)
+{
+    __asm__ __volatile__ ("outw %w0,$0x80": :"a" (value));
+}
+
+
+// The code to switch to 32-bit mode and start the kernel.
+extern void trampoline(unsigned long, void *);
+
+
+// Reserve some space for the EFI memory map.
+// Danger Will Robinson: this is just a guess at the size and alignment. If
+// it's too small, the EFI GetMemoryMap() call will fail.
+// FIXME: Make the size dynamic? Retry with larger size on failure?
+static unsigned char mmap_buf[0x2000] __attribute__ ((aligned(0x200)));
+
+// Parameters that we're given by the BIOS
+typedef struct cros_boot_info {
+    UINTN drive_number;                 // 0 - 25
+    UINTN partition_number;             // 1 - 99
+    UINTN original_address;             // our RAM address prior to execution
+    // The guid stuff was added later, so we need to consider it optional, at
+    // least for testing.
+    uint8_t partition_guid[16];         // kernel partition GUID
+} cros_boot_info_t;
+
+
+// Here's the entry point. It will be loaded by the BIOS as a standard EFI
+// application, which means it will be relocated.
+EFI_STATUS efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
+{
+    UINTN mmap_size = sizeof(mmap_buf);
+    UINTN mmap_key = 0;
+    UINTN desc_size = 0;
+    UINT32 desc_version = 0;
+    EFI_LOADED_IMAGE *loaded_image;
+    EFI_GUID loaded_image_protocol = LOADED_IMAGE_PROTOCOL;
+    EFI_GUID zero_guid = {0, 0, 0, {0, 0, 0, 0, 0, 0, 0, 0}};
+    void *guid_ptr;
+
+    // I'm here.
+    port80w(0xb0b0);
+
+    // Find the parameters that the BIOS has passed to us.
+    if (uefi_call_wrapper(systab->BootServices->HandleProtocol, 3,
+                          image,
+                          &loaded_image_protocol,
+                          &loaded_image) != 0)
+    {
+        uefi_call_wrapper(systab->ConOut->OutputString, 3, systab->ConOut,
+                          L"Can't locate protocol\r\n");
+        goto fail;
+    }
+    cros_boot_info_t *booting = loaded_image->LoadOptions;
+    if (loaded_image->LoadOptionsSize < 40) // DWR: min size including guid
+        guid_ptr = &zero_guid;
+    else
+        guid_ptr = booting->partition_guid;
+
+    // Find the parameters that we're passing to the kernel.
+    struct hacked_params *params = find_params_struct(booting->original_address);
+
+    // Update the kernel command-line string with the correct rootfs device
+    update_cmdline_inplace((char *)(unsigned long)(params->cmd_line_ptr),
+                           booting->drive_number,
+                           booting->partition_number + 1,
+                           guid_ptr);
+
+    // Obtain the EFI memory map.
+    if (uefi_call_wrapper(systab->BootServices->GetMemoryMap, 5,
+                          &mmap_size, mmap_buf, &mmap_key,
+                          &desc_size, &desc_version) != 0)
+    {
+        uefi_call_wrapper(systab->ConOut->OutputString, 2, systab->ConOut,
+                          L"Can't get memory map\r\n");
+        goto fail;
+    }
+
+    // Update the pointers to the EFI memory map and system table.
+    params->v0206_efi_signature = ('4' << 24 | '6' << 16 | 'L' << 8 | 'E');
+    params->v0206_efi_system_table = (uint32_t) (unsigned long)systab;
+    params->v0206_efi_mem_desc_size = desc_size;
+    params->v0206_efi_mem_desc_version = desc_version;
+    params->v0206_efi_mmap = (uint32_t) (unsigned long)mmap_buf;
+    params->v0206_efi_mmap_size = mmap_size;
+    params->v0206_efi_mmap_hi = (uint32_t)((uint64_t)mmap_buf >> 32);
+    params->v0206_efi_system_table_hi = (uint32_t) ((uint64_t)systab >> 32);
+
+
+    // Done with BIOS.
+    if (uefi_call_wrapper(systab->BootServices->ExitBootServices, 2,
+                          image, mmap_key) != 0)
+    {
+        uefi_call_wrapper(systab->ConOut->OutputString, 2, systab->ConOut,
+                          L"Can't exit boot services\r\n");
+        goto fail;
+    }
+
+
+    // Trampoline to 32-bit entry point. Should never return.
+    trampoline(params->code32_start, params);
+
+fail:
+
+    // Bad Things happened.
+    port80w(0xeeee);
+
+    return EFI_LOAD_ERROR;
+}

bootstub/trampoline.S
@@ -1,0 +1,112 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2009  Free Software Foundation, Inc.
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+	.p2align	4	/* force 16-byte alignment */
+        .globl trampoline
+trampoline:
+	cli
+	/* %rdi contains protected memory start and %rsi
+	contains real memory start. */
+
+	mov %rsi, %rbx
+
+	call base
+base:
+	pop %rsi
+
+	lea (cont1 - base) (%rsi, 1), %rax
+	mov %eax, (jump_vector - base) (%rsi, 1)
+
+	lea (gdt - base) (%rsi, 1), %rax
+	mov %rax, (gdtaddr - base) (%rsi, 1)
+
+	/* Switch to compatibility mode. */
+
+	lidt (idtdesc - base) (%rsi, 1)
+	lgdt (gdtdesc - base) (%rsi, 1)
+
+	/* Update %cs. Thanks to David Miller for pointing this mistake out. */
+	ljmp *(jump_vector - base) (%rsi, 1)
+
+cont1:
+	.code32
+
+	/* Update other registers. */
+	mov $0x18, %eax
+	mov %eax, %ds
+	mov %eax, %es
+	mov %eax, %fs
+	mov %eax, %gs
+	mov %eax, %ss
+
+	/* Disable paging. */
+	mov %cr0, %eax
+	and $0x7fffffff, %eax
+	mov %eax, %cr0
+
+	/* Disable amd64. */
+	mov $0xc0000080, %ecx
+	rdmsr
+	and $0xfffffeff, %eax
+	wrmsr
+
+	/* Turn off PAE. */
+	movl %cr4, %eax
+	and $0xffffffcf, %eax
+	mov %eax, %cr4
+
+	jmp cont2
+cont2:
+	.code32
+
+	mov %ebx, %esi
+
+	jmp *%edi
+
+	/* GDT. */
+	.p2align 4
+gdt:
+	/* NULL.  */
+	.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+	/* Reserved.  */
+	.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+	/* Code segment.  */
+	.byte 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x9A, 0xCF, 0x00
+
+	/* Data segment.  */
+	.byte 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x92, 0xCF, 0x00
+
+gdtdesc:
+	.word 31
+gdtaddr:
+	.quad gdt
+
+idtdesc:
+	.word 0
+idtaddr:
+	.quad 0
+
+	.p2align 4
+jump_vector:
+	/* Jump location. Is filled by the code */
+	.long 0
+	.long 0x10
+

chromebook-config.sh
@@ -8,6 +8,8 @@ TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-2018.
 ARM64_DEBIAN_ROOTFS_URL="https://people.collabora.com/~eballetbo/debian/images/debian-stretch-chromebook-arm64.tar.gz"
 ARM64_TOOLCHAIN="gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu"
 ARM64_TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-2018.08/$ARM64_TOOLCHAIN.tar.xz"
+# x86_64 rootfs
+X86_64_DEBIAN_ROOTFS_URL="https://people.collabora.com/~eballetbo/debian/images/debian-stretch-chromebook-amd64.tar.gz"
 
 KERNEL_URL="git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 

commit 71973ee8ae547dd5d0cef09461feffc0f77a6542

chromebook-setup: Fix hardcoded use of arm64 kernel for arm boards
Fix the path to get the kernel for arm boards.

Fixes: 50da924eceb9 ("Add initial support for x86_64 based Chromebooks")
Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

chromebook-setup.sh
@@ -286,7 +286,7 @@ create_fit_image()
          fi
 
          mkimage -D "-I dts -O dtb -p 2048" -f auto -A arm64 -O linux -T kernel -C lz4 -a 0 \
-                 -d arch/arm64/boot/Image.lz4 $dtbs \
+                 -d arch/${CB_SETUP_ARCH}/boot/Image.lz4 $dtbs \
                  kernel.itb
     else
 	echo "TODO: create x86_64 FIT image, now using a raw image"