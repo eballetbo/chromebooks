eballetbo/chromebooks

commit 71973ee8ae547dd5d0cef09461feffc0f77a6542

chromebook-setup: Fix hardcoded use of arm64 kernel for arm boards
Fix the path to get the kernel for arm boards.

Fixes: 50da924eceb9 ("Add initial support for x86_64 based Chromebooks")
Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

chromebook-setup.sh
@@ -286,7 +286,7 @@ create_fit_image()
          fi
 
          mkimage -D "-I dts -O dtb -p 2048" -f auto -A arm64 -O linux -T kernel -C lz4 -a 0 \
-                 -d arch/arm64/boot/Image.lz4 $dtbs \
+                 -d arch/${CB_SETUP_ARCH}/boot/Image.lz4 $dtbs \
                  kernel.itb
     else
 	echo "TODO: create x86_64 FIT image, now using a raw image"

commit c65294b09181f1ac1d1b6432ac50f80468d03d38

chromebook-setup: Fix mkimage architecture for arm boards
Use the correct architecture when creating the mkimage.

Fixes: 71973ee8ae54 ("chromebook-setup: Fix hardcoded use of arm64 kernel for arm boards")
Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

chromebook-setup.sh
@@ -285,7 +285,7 @@ create_fit_image()
              dtbs="-b arch/arm64/boot/dts/rockchip/rk3399-gru-kevin.dtb"
          fi
 
-         mkimage -D "-I dts -O dtb -p 2048" -f auto -A arm64 -O linux -T kernel -C lz4 -a 0 \
+         mkimage -D "-I dts -O dtb -p 2048" -f auto -A ${CB_SETUP_ARCH} -O linux -T kernel -C lz4 -a 0 \
                  -d arch/${CB_SETUP_ARCH}/boot/Image.lz4 $dtbs \
                  kernel.itb
     else