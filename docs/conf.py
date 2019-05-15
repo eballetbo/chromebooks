eballetbo/chromebooks

commit 54695931b1ee13f4babd3039e901f971515c385d

docs: Use default template for rtd and set a local template
Use the sphinx_rtd_theme for building the docs locally.

Signed-off-by: Enric Balletbo i Serra <eballetbo@gmail.com>

docs/conf.py
@@ -74,7 +74,15 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

commit 61a0be8a2718c0b02cad5aeee337f975dc02f3d0

docs: Fix build error by importing os
The previous commit introduced a build error due it was using os and was
not imported. Fix this by adding an 'import os' on the top of the file.

Fixes: 54695931b1e ('docs: Use default template for rtd and set a local template')
Signed-off-by: Enric Balletbo i Serra <eballetbo@gmail.com>

docs/conf.py
@@ -12,7 +12,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 