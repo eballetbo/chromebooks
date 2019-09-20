eballetbo/chromebooks

commit 966ecfece16f39967e0fdb5a4e99620d3b8770f6

chromebook-setup: Simplify the step to sign the kernel
Now that we need to sign the kernel for different architectures try to
simplify a bit hwo this step is done.

Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

chromebook-setup.sh
@@ -483,23 +483,37 @@ cmd_build_bootstub()
 
 cmd_build_vboot()
 {
+    local arch
+    local bootloader
+    local vmlinuz
+
     echo "Sign the kernels to boot with Chrome OS devices..."
 
-    local boot=kernel/kernel.vboot
-    # Install it on the boot partition
-    if [ "$CB_SETUP_ARCH" == "x86_64" ]; then
-        echo "root=PARTUUID=%U/PARTNROFF=1 rootwait rw" > boot_params
-        [ -f bootstub/bootstub.efi ] || cmd_build_bootstub
-        sudo vbutil_kernel --pack "$boot" \
-                           --keyblock /usr/share/vboot/devkeys/kernel.keyblock \
-                           --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk \
-                           --version 1 --config boot_params \
-                           --bootloader ./bootstub/bootstub.efi \
-                           --vmlinuz kernel/arch/x86/boot/bzImage
-    else
-        echo "init=/sbin/init root=PARTUUID=%U/PARTNROFF=1 rootwait rw noinitrd" > boot_params
-        sudo vbutil_kernel --pack "$boot" --keyblock /usr/share/vboot/devkeys/kernel.keyblock --version 1 --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk --bootloader boot_params --config boot_params --vmlinuz kernel/kernel.itb --arch arm
-    fi
+    case "$CB_SETUP_ARCH" in
+        arm|arm64)
+            arch="arm"
+            bootloader="boot_params"
+            vmlinuz="kernel/kernel.itb"
+            ;;
+        x86_64)
+            arch="x86"
+            bootloader="./bootstub/bootstub.efi"
+            vmlinuz="kernel/arch/x86/boot/bzImage"
+            ;;
+        *)
+            echo "Unsupported vboot architecture"
+	    exit 1
+            ;;
+    esac
+
+    echo "root=PARTUUID=%U/PARTNROFF=1 rootwait rw" > boot_params
+    sudo vbutil_kernel --pack kernel/kernel.vboot \
+                       --keyblock /usr/share/vboot/devkeys/kernel.keyblock \
+                       --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk \
+                       --version 1 --config boot_params \
+                       --bootloader $bootloader \
+                       --vmlinuz $vmlinuz \
+                       --arch $arch
 
     echo "Done."
 }

commit 2a8d634680ee48c997f8b884c26d750a5a23c902

chromebook-setup: Reinstate the bootstub
Removed by error build the bootstub if is needed.

Fixes: 966ecfece16f3 ("chromebook-setup: Simplify the step to sign the kernel")
Reported-by: Emil Velikov <emil.velikov@collabora.com>
Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

chromebook-setup.sh
@@ -511,6 +511,7 @@ cmd_build_vboot()
             ;;
         x86_64)
             arch="x86"
+            [ -f ./bootstub/bootstub.efi ] || cmd_build_bootstub
             bootloader="./bootstub/bootstub.efi"
             vmlinuz="kernel/arch/x86/boot/bzImage"
             ;;