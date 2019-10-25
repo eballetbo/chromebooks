0dc685dd908c8ee93f0e923e195ffc13204215de
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2019-10-25 11:23:54 +0200

    tools: testing: test-pwm-backlight: Adapt the script for LAVA
    Use the LAVA helpers in order to signal LAVA on success or failure.
    
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- tools/testing/test-pwm-backlight.sh
+++ tools/testing/test-pwm-backlight.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+. lava-test-report
+
+TEST_CASE_ID="$1"
+
+if [ -z "${TEST_CASE_ID}" ]; then
+	echo "Usage: $0 <test-case-id>"
+	exit 1
+fi
+
 echo $(expr $(cat /sys/class/backlight/backlight/max_brightness) / 2) > /sys/class/backlight/backlight/brightness
 
 if [ $(cat /sys/kernel/debug/pwm | grep backlight | sed 's/.*duty: //' | cut -d ' ' -f1) -eq "0" ]
 then
-        echo "FAIL"
-else
-        echo "PASS"
+	test_report_exit fail
 fi
 
-exit 0
+test_report_exit pass
+
