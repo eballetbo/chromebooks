4a76cb12ee623e347f63df241e6116d48789c50e
Fabien Lahoudere <fabien.lahoudere@collabora.com>
2019-11-20 12:12:55 +0100

    testing: test_cros_ec_rtc_abi: Test RTC ABI
    The script checks the RTC ABI that all required files are present.
    
    Signed-off-by: Fabien Lahoudere <fabien.lahoudere@collabora.com>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- tools/testing/test-cros-ec.py
+++ tools/testing/test-cros-ec.py
@@ -321,6 +321,22 @@ class TestCrosEC(unittest.TestCase):
         if match == 0:
             self.skipTest("No extcon device found, skipping")
 
+    def test_cros_ec_rtc_abi(self):
+        if not is_feature_supported(EC_FEATURE_RTC):
+            self.skipTest("EC_FEATURE_RTC not supported, skipping")
+        match = 0
+        for devname in os.listdir("/sys/class/rtc"):
+            fd = open("/sys/class/rtc/" + devname + "/name", 'r')
+            devtype = fd.read()
+            fd.close()
+            if devtype.startswith("cros-ec-rtc"):
+                files = [ "date", "hctosys", "max_user_freq", "since_epoch",
+                          "time", "wakealarm" ]
+                match += 1
+                for filename in files:
+                    self.assertEqual(os.path.exists("/sys/class/rtc/" + devname + "/" + filename), 1)
+        self.assertNotEqual(match,0)
+
 if __name__ == '__main__':
     unittest.main(testRunner=LavaTestRunner(),
         # these make sure that some options that are not applicable
