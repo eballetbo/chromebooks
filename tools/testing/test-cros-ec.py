ac2bb295fb0f8fddd44dca26b9c445cab1596db6
Fabien Lahoudere <fabien.lahoudere@collabora.com>
2019-11-28 12:27:48 +0100

    testing: cros-ec: Add helper to convert kernel release
    Add one function to convert version major and minor
    to an integer.
    Add a function to get the running kernel version and
    convert it using the previous function.
    
    Signed-off-by: Fabien Lahoudere <fabien.lahoudere@collabora.com>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- tools/testing/test-cros-ec.py
+++ tools/testing/test-cros-ec.py
@@ -131,6 +131,28 @@ def read_file(name):
     fd.close()
     return contents
 
+# Return an int froom kernel version to allow to compare
+def version_to_int(version, major, minor):
+    pattern = "{0:03d}{1:03d}{2:03d}"
+    return int(pattern.format(version, major, minor))
+
+# Return the running kernel version
+def current_kernel_version():
+    fd = open("/proc/version", 'r')
+    current = fd.read().split()[2].split('-')[0].split('.')
+    fd.close()
+    return version_to_int(int(current[0]), int(current[1]), int(current[2]))
+
+def kernel_lower_than(version, major, minor):
+    if version_to_int(version, major, minor) > current_kernel_version():
+        return True
+    return False
+
+def kernel_greater_than(version, major, minor):
+    if version_to_int(version, major, minor) < current_kernel_version():
+        return True
+    return False
+
 ###############################################################################
 # TEST RUNNERS
 ###############################################################################
